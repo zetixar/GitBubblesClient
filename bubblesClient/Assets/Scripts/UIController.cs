6424d28876cf78b7d9d6144f3edacbb31a30afd5
zetixar <mohsendp@gmail.com>
2016-02-27 14:03:29 -0500

    sprite added

--- bubblesClient/Assets/Scripts/UIController.cs
+++ bubblesClient/Assets/Scripts/UIController.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-public class MiniCameraController : MonoBehaviour {
+using UnityEngine.EventSystems;
+public class UIController : MonoBehaviour {
 
 	Camera mainCam;
 	public Camera miniCam;
 	public Button camLockBtn;
+	public Button blessingModeBtn;
+	public Button pushLinkBtn;
 	// Use this for initialization
 	void Start () {
 		mainCam = Camera.main;
@@ -26,6 +29,19 @@ public class MiniCameraController : MonoBehaviour {
 	{
 		netClientMgr.GOspinner.cameraFollowMynode = !netClientMgr.GOspinner.cameraFollowMynode;
 		camLockBtn.image.color = netClientMgr.GOspinner.cameraFollowMynode ? Color.green : Color.grey;
+	}
+
+	public void BlessingMode(int ZeroOffOneOn)
+	{
+		netClientMgr.GOspinner.blessingMode = (ZeroOffOneOn == 0) ? false : true;
+		blessingModeBtn.image.color = netClientMgr.GOspinner.blessingMode ? Color.blue : Color.grey;
+		
+	}
+
+	public void PushLinkMode(int ZeroOffOneOn)
+	{
+		netClientMgr.GOspinner.pushLinkMode = (ZeroOffOneOn == 0) ? false : true;
+		pushLinkBtn.image.color = netClientMgr.GOspinner.pushLinkMode ? Color.blue : Color.grey;
 	}
 
 }
